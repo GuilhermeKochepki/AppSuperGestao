8dcd853d483223915fb6863c9aeb88b820b16499
GuilhermeKochepki <gui.kochepki@gmail.com>
2021-08-03 11:15:14 -0300

    Realizado inserções no Banco de Dados

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -12,5 +12,7 @@ class Fornecedor extends Model
     //trocando o nome da tabela, para não dar erro no Eloquent, 
     //que reconheceria o nome como fornecedors
     protected $table = 'fornecedores';
+    //adicionando o fillable para inserir usando o create com arrays associativos
+    //exemplo \App\Model\Fornecedor::create(['nome'=>'Fornecedor ABC', 'site'=>'fornecedorabc.com.br', 'uf'=>'SP', 'email'=>'contato@fornecedorabc.com.br']);
     protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
