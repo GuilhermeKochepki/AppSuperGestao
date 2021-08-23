6bfd55dd13aac18f96927ca888560b02ab8ad9e4
GuilhermeKochepki <gui.kochepki@gmail.com>
2021-08-23 19:39:43 -0300

    Inserindo SoftDelete na table Fornecedor

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     //trocando o nome da tabela, para não dar erro no Eloquent, 
     //que reconheceria o nome como fornecedors
