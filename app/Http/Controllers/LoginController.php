de855378d8c1b620f8c2edc02ac6d2cb0c0b7579
GuilhermeKochepki <gui.kochepki@gmail.com>
2021-09-15 11:46:13 -0300

    Finalizado o projeto base, com princípios básicos do laravel

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -66,10 +66,15 @@ class LoginController extends Controller
 
             //dd($_SESSION);
 
-            return redirect()->route('app.clientes');
+            return redirect()->route('app.home');
         } else {
             return redirect()->route('site.login', ['erro' => '1']);
         }
 
+    }
+
+    public function sair() {
+        session_destroy();
+        return redirect()->route('site.index');
     }
 }
